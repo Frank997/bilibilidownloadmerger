f8faf868cefb2f13fb7848e384d3293c378a6fac
rerik <noreplay@noreplayLIenkgiewnKGN.com>
2023-11-26 16:44:25 +0800

    修改视频文件输出结构为：视频文件夹->分p视频

--- mergeBiliDown.go
+++ mergeBiliDown.go
@@ -18,6 +18,7 @@ for test:
 */
 // TODO 如果所有entry.json键排序固定的话，可以改成一个正则，一次匹配出这几个元素，但如果性能不是问题，分别匹配更保险，即使entry.json里键排序不固定也不会出错
 var regexDownTitle = regexp.MustCompile(`^.*"download_subtitle":"([^"]*)["].*$`) //优先选这个名字，因为这个名字更长
+var regexPart = regexp.MustCompile(`^.*"part":"([^"]*)["].*$`)                   // 分p名
 var regexTitle = regexp.MustCompile(`^.*"title":"([^"]*)["].*$`)                 //[^",] 作用是匹配不包含引号和逗号的所有字符
 var regexBvid = regexp.MustCompile(`^.*"bvid":"([^,"}]*)[,"}].*$`)
 var regexAvid = regexp.MustCompile(`^.*"avid":([^,"}]*)[,"}].*$`)                  //avid 是数字，其实不需要引号
@@ -32,7 +33,7 @@ func main() {
 	}
 
 	//获取输入输出路径
-	rootDir, err := filepath.Abs(os.Args[1])
+	inDir, err := filepath.Abs(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
@@ -41,12 +42,26 @@ func main() {
 		panic(err)
 	}
 	//为输入输出路径末尾添加路径分隔符
-	rootDir += string(os.PathSeparator)
+	inDir += string(os.PathSeparator)
 	outDir += string(os.PathSeparator)
+
+	//检查输入和输出路径
+	//输入路径必须存在，输出路径必须不存在
+	inDirExists, _ := exists(inDir)
+	if !inDirExists {
+		fmt.Println("错误！输入目录不存在！")
+		os.Exit(-2)
+	}
+
+	outDirExists, _ := exists(outDir)
+	if outDirExists{
+		fmt.Println("错误！输出目录已存在！请将输出目录设置为尚未创建的路径！")
+		os.Exit(-3)
+	}
 
 	var files []string
 	//取出所有video.m4s路径
-	err = filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+	err = filepath.Walk(inDir, func(path string, info os.FileInfo, err error) error {
 		if strings.HasSuffix(path, "video.m4s") {
 			files = append(files, path)
 		}
@@ -77,55 +92,115 @@ func main() {
 		}
 	}
 
+	fmt.Println("正在合并视频，时长取决于您的视频数目，可能需要数分钟，请等待......")
+	
 	//创建输出目录
 	os.MkdirAll(outDir, os.ModePerm)
 
+	//为每个视频创建目录
+	//key:bvid, value:视频标题
+	dirMap := make(map[string]string)
+
 	for _, file := range files {
-		audio := file[0:len(file)-vidM4sSuffixLength] + "audio.m4s"                    //audio.m4s和video.m4s在同一目录
-		vidName := getVidName(file[0:len(file)-vidM4sSuffixLength] + "..\\entry.json") //entry.json在video.m4s的上级目录
-		isSucceed := merge(ffmpegBin, audio, file, outDir+vidName+".mp4")
+		currentFilePath:=file[0:len(file)-vidM4sSuffixLength]
+		audio :=  currentFilePath+ "audio.m4s" //audio.m4s和video.m4s在同一目录
+		
+		//read entry.json file content as str
+		// the path is: currentFilePath/../entry.json
+		content, err := ioutil.ReadFile(currentFilePath + ".." + string(os.PathSeparator) + "entry.json")
+		if err != nil {
+			panic(err)
+		}
+		contentStr := string(content)
+
+		//如果尚未创建当前视频输出目录，则创建，否则获取当前视频输出目录
+		currentVidOutDir := createDirIfNeed(contentStr, outDir, dirMap)
+
+		vidName := getVidName(contentStr) //entry.json在video.m4s的上级目录
+		isSucceed := merge(ffmpegBin, audio, file, currentVidOutDir+vidName+".mp4")
 		// fmt.Println(file)
 		if isSucceed {
 			vidCounts++
 		}
+
+		//每处理50个视频，提示一下用户
+		if vidCounts%50==0 {
+			fmt.Println("已成功处理 ", vidCounts , " 个视频，程序仍在运行，请等待......")
+		}
+
 	}
 
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
+	fmt.Println()
 	//打印结果
 	fmt.Println("程序执行完毕：")
 	fmt.Println("视频总数：", len(files))
-	fmt.Println("成功处理：", vidCounts)
+	fmt.Println("处理成功：", vidCounts)
+
+	faildCount := len(files) - vidCounts
+	fmt.Println("处理失败：", faildCount)
 	if len(files) > vidCounts {
-		fmt.Println("有", len(files)-vidCounts, "个视频合并失败，请查看控制台输出。")
+		fmt.Println("有", faildCount, "个视频合并失败，请查看控制台输出。")
 	}
 }
 
-func getVidName(entryPath string) string {
-	content, err := ioutil.ReadFile(entryPath)
-	if err != nil {
-		panic(err)
+// 参数为entry.json文件内容、输出根目录名、bvid和视频标题map，返回值为当前json文件关联视频的输出目录名
+func createDirIfNeed(contentStr string, outDir string, dirMap map[string]string) string {
+	//find bvid
+	bvid:=regexBvid.FindStringSubmatch(contentStr)[1]
+	//尝试取出bvid关联的目录，一个视频下的多个分p拥有相同bvid，所以它们会被存入同一目录下
+	dirPath, success:=dirMap[bvid]
+	//如果获取到目录，直接返回，否则创建目录并存入map
+	if success{
+		return dirPath
+	}else {
+		//目录名为: 视频标题_bvid_avid
+		//用视频标题作为目录名
+		vidTitle :=regexTitle.FindStringSubmatch(contentStr)[1]
+		avid:=regexAvid.FindStringSubmatch(contentStr)[1]
+		//拼接目录名
+		vidDir:=outDir+vidTitle+"_"+bvid+"_"+avid+string(os.PathSeparator)
+		//创建目录
+		os.MkdirAll(vidDir, os.ModePerm)
+		//存入map，之后可用bvid查找到此目录
+		dirMap[bvid] = vidDir
+		//返回目录名
+		return vidDir
 	}
-	contentStr := string(content)
+}
+
+func getVidName(contentStr string) string {
+
+
 	// get video title
 	// first, try get download title, if nil, get the title. btw: download title has a detail name of the video, such as: videoName partName, but the title only has videoName
-	match := regexDownTitle.FindStringSubmatch(contentStr)
-	if len(match) < 2 {
-		match = regexTitle.FindStringSubmatch(contentStr)
-	}
+	// match := regexDownTitle.FindStringSubmatch(contentStr)
+	// if len(match) < 2 {
+	// 	match = regexTitle.FindStringSubmatch(contentStr)
+	// }
+
+	//获取分p名
+	part := regexPart.FindStringSubmatch(contentStr)[1]
 	// fmt.Println(match[0])
 	// match[0] is raw string, regex group match from index 1
-	title := match[1]
+	// title := match[1]
 	//get bvid
-	match = regexBvid.FindStringSubmatch(contentStr)
-	bvid := match[1]
+	// match = regexBvid.FindStringSubmatch(contentStr)
+	// bvid := match[1]
 	// println(title, bvid)
 	//get avid
-	match = regexAvid.FindStringSubmatch(contentStr)
-	avid := match[1]
+	// match = regexAvid.FindStringSubmatch(contentStr)
+	// avid := match[1]
 	//get download timestamp. side effect is make the videoname unique, so need't gen a random for videoName
-	match = regexDownTime.FindStringSubmatch(contentStr)
-	downTime := match[1]
+	downTime := regexDownTime.FindStringSubmatch(contentStr)[1]
+	// downTime := match[1]
 	//gen filename for output video
-	filename := title + "_" + bvid + "_" + avid + "_" + downTime
+	// filename := title + "_" + bvid + "_" + avid + "_" + downTime
+	//视频标题为：分p名_下载时间
+	filename := part + "_" + downTime
 	// fmt.Println(filename)
 	//remove windows illegal chars for filename
 	filename = regexReplaceWindowsIllegalChars.ReplaceAllString(filename, "")
@@ -148,4 +223,12 @@ func merge(ffmpegBin string, audio string, video string, outFile string) bool {
 		return !isSucceed
 	}
 	return isSucceed
+}
+
+// exists returns whether the given file or directory exists
+func exists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil { return true, nil }
+    if os.IsNotExist(err) { return false, nil }
+    return false, err
 }
